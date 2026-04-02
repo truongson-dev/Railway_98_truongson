fcb5f35dfbc1bc6ebe2eb809c95b572e187efee5
TRƯƠNG THANH SƠN <son.truong27@student.passerellesnumeriques.org>
2026-04-02 22:05:51 +0700

    Delete R98_Lab3_AccountManagement directory

--- R98_Lab3_AccountManagement/src/main/java/com/vti/repository/IPossitionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vti.entity.Position;
-
-public interface IPossitionRepository extends JpaRepository<Position, Short> {
-
-}
