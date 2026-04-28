30268059d1ca65d6f0aef352b4dd37b17030988f
TRƯƠNG THANH SƠN <son.truong27@student.passerellesnumeriques.org>
2026-04-28 21:08:49 +0700

    Add files via upload

---
+++ Fontend_HTML_CSS/ReactJS/1. TypeScript/LAB7/Demo1.js
@@ -0,0 +1,36 @@
+"use strict";
+//  Viết 1 chương trình tính toán
+// Phép cộng 2 số a và b
+function sum(a, b) {
+    return a + b;
+}
+// Phép trừ 2 số a và b
+function minus(a, b) {
+    return a - b;
+}
+// Phép nhân 2 số a và b
+function multiply(a, b) {
+    return a * b;
+}
+// Phép chia 2 số a và b
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Không thể chia cho 0");
+    }
+    return a / b;
+}
+//Demo bình thường
+console.log(`Kết quả của sum(5, 10) là: ${sum(5, 10)}`); // Kết quả của sum(5, 10) là: 15
+console.log(`Kết quả của minus(5, 10) là: ${minus(5, 10)}`); // Kết quả của minus(5, 10) là: -5
+console.log(`Kết quả của multiply(5, 10) là: ${multiply(5, 10)}`); // Kết quả của multiply(5, 10) là: 50
+console.log(`Kết quả của divide(5, 10) là: ${divide(5, 10)}`); // Kết quả của divide(5, 10) là: 0.5
+console.log(`------------------------------`);
+function calculate(a, b, f) {
+    let result = f(a, b);
+    return result;
+}
+//Demo sử dụng hàm calculate
+console.log(`Kết quả của calculate(5, 10, sum) là: ${calculate(5, 10, sum)}`); // Kết quả của calculate(5, 10, sum) là: 15
+console.log(`Kết quả của calculate(5, 10, minus) là: ${calculate(5, 10, minus)}`); // Kết quả của calculate(5, 10, minus) là: -5
+console.log(`Kết quả của calculate(5, 10, multiply) là: ${calculate(5, 10, multiply)}`); // Kết quả của calculate(5, 10, multiply) là: 50
+console.log(`Kết quả của calculate(5, 10, divide) là: ${calculate(5, 10, divide)}`); // Kết quả của calculate(5, 10, divide) là: 0.5
---
+++ Fontend_HTML_CSS/ReactJS/1. TypeScript/LAB8/Demo1.ts
@@ -0,0 +1,100 @@
+// Demo Promise trong TypeScript
+// Mô phỏng chương trình tạo mới Account
+// Check Email đã tồn tại hay chưa, nếu tồn tại thì trả về lỗi
+// Check Username đã tồn tại hay chưa, nếu tồn tại thì trả về lỗi
+// Tạo Account
+
+// Viết hàm checkEmail
+function callAPICheckEmail() {
+  console.log("Call API check email...");
+
+  let promise_CheckEmail = new Promise(function (resolve, reject) {
+    //   url: 'http://localhost:8080/api/v1/accounts/checkEmailExits',
+    // 			type: 'GET',
+    // 			data: JSON.stringify(account), // body
+    // 			contentType: "application/json", // type of body (json, xml, text)
+    // 			// dataType: 'json', // datatype return
+    // 			beforeSend: function(xhr) {
+    // 				xhr.setRequestHeader("Authorization", "Basic " + btoa(localStorage.getItem("USERNAME") + ":" + localStorage.getItem("PASSWORD")));
+    // 			},
+
+    let checkEmail = false;
+    if (checkEmail) {
+      reject("Email đã tồn tại, vui lòng chọn email khác!");
+    } else {
+      resolve("Email hợp lệ, có thể sử dụng để tạo mới Account!");
+    }
+  });
+  return promise_CheckEmail;
+}
+
+function callAPICheckUsername() {
+  console.log("Call API check Username...");
+  //   url: 'http://localhost:8080/api/v1/accounts/checkEmailExits',
+  // 			type: 'GET',
+  // 			data: JSON.stringify(account), // body
+  // 			contentType: "application/json", // type of body (json, xml, text)
+  // 			// dataType: 'json', // datatype return
+  // 			beforeSend: function(xhr) {
+  // 				xhr.setRequestHeader("Authorization", "Basic " + btoa(localStorage.getItem("USERNAME") + ":" + localStorage.getItem("PASSWORD")));
+  // 			},
+
+  let promise_CheckUsername = new Promise(function (resolve, reject) {
+    //   url: 'http://localhost:8080/api/v1/accounts/checkEmailExits',
+    // 			type: 'GET',
+    // 			data: JSON.stringify(account), // body
+    // 			contentType: "application/json", // type of body (json, xml, text)
+    // 			// dataType: 'json', // datatype return
+    // 			beforeSend: function(xhr) {
+    // 				xhr.setRequestHeader("Authorization", "Basic " + btoa(localStorage.getItem("USERNAME") + ":" + localStorage.getItem("PASSWORD")));
+    // 			},
+
+    let checkUsername = false;
+    if (checkUsername) {
+      reject("Uesrname đã tồn tại, vui lòng chọn Uesrname khác!");
+    } else {
+      resolve("Uesrname hợp lệ, có thể sử dụng để tạo mới Account!");
+    }
+  });
+  return promise_CheckUsername;
+}
+
+function callAPICreateNewAccount() {
+  console.log("Call API createNewAccount...");
+  let promise_createAccount = new Promise(function (resolve, reject) {
+    //   url: 'http://localhost:8080/api/v1/accounts',
+    // 			type: 'GET',
+    // 			data: JSON.stringify(account), // body
+    // 			contentType: "application/json", // type of body (json, xml, text)
+    // 			// dataType: 'json', // datatype return
+    // 			beforeSend: function(xhr) {
+    // 				xhr.setRequestHeader("Authorization", "Basic " + btoa(localStorage.getItem("USERNAME") + ":" + localStorage.getItem("PASSWORD")));
+    // 			},
+    let createNewAccount = false;
+    if (createNewAccount) {
+      reject("tạo mới Account thất bại, vui lòng thử lại!");
+    } else {
+      resolve("Tạo mới Account thành công");
+    }
+  });
+  return promise_createAccount;
+}
+
+// Kết quả không chính xác
+// callAPICheckEmail();
+// callAPICheckUsername();
+// callAPICreateNewAccount();
+
+callAPICheckEmail()
+  .then(function (msg) {
+    return callAPICheckUsername();
+  })
+  .then(function (msg) {
+    return callAPICreateNewAccount();
+  })
+  .then(function (msg) {
+    console.log(msg);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
